f5f8b1bd19db4ecaba24c47df4ca7758f7864aba
Mohammed Abu Awwad <mohammedabuawwad1@gmail.com>
2021-05-28 09:09:29 +0300

    final 8

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,7 +26,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 <script src="{{ asset('theme/vendor/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('theme/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{!! asset('js/Chart.min.js') !!}"></script>
+<script src="{!! asset('js/chart.min.js') !!}"></script>
 {{-- <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
         <script>
 
